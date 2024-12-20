ed3c583c6543bd7518caa8e5a12245d61c12a768
BushepII <paul.13.reg@gmail.com>
2024-12-20 22:23:59 +0100

    Second version of rooter, working better by leading directly to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from './components/Header'
+import { Outlet } from "react-router";
 
-import './style/App.css'
+import Header from './components/Header.jsx';
 
 function App() {
-
   return (
     <>
       <Header />
+      <Outlet />
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,41 @@
-import '../style/components_style/Header.css'
+import { Link } from "react-router-dom";
+import '../style/components_style/Header.css';
 
-function Header() {
+const Header = () => {
 
   return (
-    <>
-        <header className='container-fluid px-0'>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-                <div className='container-fluid'>
+    <header className='container-fluid px-0'>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <div className='container-fluid'>
 
-                    <a href="#" className='navbar-brand'>JOHN DOE</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <a href="#" className='navbar-brand'>JOHN DOE</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                        <ul className='navbar-nav mb-2 mb-lg-0'>
-                            <li className='nav-item'>
-                                <a href="#" className='nav-link active' aria-current="page">ACCUEIL</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#" className='nav-link' aria-current="page">SERVICES</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#" className='nav-link' aria-current="page">RÉALISATIONS</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#" className='nav-link' aria-current="page">BLOG</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#" className='nav-link' aria-current="page">ME CONTACTER</a>
-                            </li>
-                        </ul>
-                    </div>
-
+                <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <ul className='navbar-nav mb-2 mb-lg-0'>
+                        <li className='nav-item'>
+                            <Link to="/" className='nav-link active' aria-current="page">ACCUEIL</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/services" className='nav-link' aria-current="page">SERVICES</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/realisations" className='nav-link' aria-current="page">RÉALISATIONS</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/blog" className='nav-link' aria-current="page">BLOG</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/contact" className='nav-link' aria-current="page">ME CONTACTER</Link>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </header>
-    </>
+
+            </div>
+        </nav>
+    </header>
   )
 }
 
