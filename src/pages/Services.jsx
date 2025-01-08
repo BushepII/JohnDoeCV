6a9efb98107eaba2118f108e62ec1b6f74800339
BushepII <bushepii@gmail.com>
2025-01-08 19:04:43 +0100

    first working blog page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
             <picture>
                 <source srcSet={bannerDesktop} type="image/jpg" media="(min-width: 992px)" />
                 <source srcSet={bannerTablette} type="image/jpg" media="(min-width: 768px)" />
-                <img src={bannerMobile} alt="" className="img-fluid" />
+                <img src={bannerMobile} alt="" className="img-fluid banner" />
             </picture>
             <section className="container-fluid">
                 <section className="row text-center">
@@ -23,19 +23,19 @@ const Services = () => {
                     <section className="col-12 col-md-3 my-3 card text-center">
                         <section className="card-body">
                             <h2 className="card-title">UX DESIGN</h2>
-                            <p className="card-text">L'<strong>UX Design</strong> est une méthode de conception centrée sur l'utilisateur. Son but est d'offrir une expérience de navigation optimale à l'internaute.</p>
+                            <p className="card-text">L&apos;<strong>UX Design</strong> est une méthode de conception centrée sur l&apos;utilisateur. Son but est d&apos;offrir une expérience de navigation optimale à l&apos;internaute.</p>
                         </section>
                     </section>
                     <section className="col-12 col-md-3 my-3 card text-center">
                         <section className="card-body">
                             <h2 className="card-title">DÉVELOPPEMENT WEB</h2>
-                            <p className="card-text">Le <strong>Développement de sites web</strong> repose sur l'utilisation des langages HTML, CSS, JavaScript et PHP.</p>
+                            <p className="card-text">Le <strong>Développement de sites web</strong> repose sur l&apos;utilisation des langages HTML, CSS, JavaScript et PHP.</p>
                         </section>
                     </section>
                     <section className="col-12 col-md-3 my-3 card text-center">
                         <section className="card-body">
                             <h2 className="card-title">RÉFÉRENCEMENT</h2>
-                            <p className="card-text">Le <strong>Référencement naturel d'un site</strong>, aussi appelé SEO, consiste à mettre des techniques en oeuvre pour <emphasize>améliorer sa position</emphasize> dans les résultats des moteurs de recherche.</p>
+                            <p className="card-text">Le <strong>Référencement naturel d&apos;un site</strong>, aussi appelé SEO, consiste à mettre des techniques en oeuvre pour <i>améliorer sa position</i> dans les résultats des moteurs de recherche.</p>
                         </section>
                     </section>
                 </section>
